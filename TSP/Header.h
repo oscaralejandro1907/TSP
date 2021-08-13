da46c77c0851a587472c1b7c5f0497ea78d95126
Oscar Alejandro Hernandez Lopez <oscarale930719@gmail.com>
2021-08-13 17:20:35 -0500

    update

---
+++ TSP/Header.h
@@ -0,0 +1,15 @@
+//
+//  Header.h
+//  TSP
+//
+//  Created by Oscar Alejandro Hernández López on 13/08/21.
+//
+
+#ifndef Header_h
+#define Header_h
+
+#include <iostream>
+
+using namespace std;
+
+#endif /* Header_h */
--- TSP/main.cpp
+++ TSP/main.cpp
@@ -5,10 +5,11 @@
 //  Created by Oscar Alejandro Hernández López on 13/08/21.
 //
 
-#include <iostream>
+
+#include "Header.h"
 
 int main(int argc, const char * argv[]) {
     // insert code here...
-    std::cout << "Hello, World!\n";
+    cout << "Hello, World!\n";
     return 0;
 }
